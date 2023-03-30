82b9c0b6c8d56b8c30cec78dcc0f9ac692466c00
ManuelDs28 <ktireds2803@gmail.com>
2023-03-30 23:57:51 +0200

    auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+const { StatusCodes } = require('http-status-codes');
+
+module.exports.isAuthenticated = (req, res, next) => {
+  const authorization = req.header('Authorization')
+  
+  if (!authorization) {
+    return next(createError(StatusCodes.UNAUTHORIZED, 'Authorization header was not provided'))
+  }
+  
+  const [schema, token] = authorization.split(' ');
+  
+  if (schema !== 'Bearer') {
+    return next(createError(StatusCodes.UNAUTHORIZED, 'Authorization schema is not supported'))
+  }
+  
+  if (!token) {
+    return next(createError(StatusCodes.UNAUTHORIZED, 'A token must be provided'))
+  }
+  
+  
+  const secret = process.env.JWT_SECRET || 'test'
+  jwt.verify(token, secret, (err, decodedToken) => {
+    console.log(token);
+    if (err) {
+      return next(err)
+    }
+    req.currentUserId = decodedToken.id
+    next()
+  })
+}
