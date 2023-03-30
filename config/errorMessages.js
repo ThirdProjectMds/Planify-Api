cd129ddab7a5dec2dc1de305cf728c4b0ee850f1
ManuelDs28 <ktireds2803@gmail.com>
2023-03-30 16:47:07 +0200

    error and db config

---
+++ config/errorMessages.js
@@ -0,0 +1,9 @@
+const REQUIRED_FIELD = 'Required field';
+const INVALID_LENGTH = 'Invalid length';
+const INVALID_EMAIL = 'Invalid email';
+
+module.exports = {
+  REQUIRED_FIELD,
+  INVALID_LENGTH,
+  INVALID_EMAIL
+}
