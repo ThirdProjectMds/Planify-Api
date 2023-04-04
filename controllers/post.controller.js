5ce5e196fc0c2b2bbec0c02b04570cf964bfcb5d
ManuelDs28 <ktireds2803@gmail.com>
2023-04-04 13:11:04 +0200

    post controller

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,5 +1,39 @@
-const mydata = require("../data.json") 
+// Post = require("../models/Post.model");
+// const { StatusCodes } = require("http-status-codes");
+const Posts = require("../data.json");
 
+module.exports.create = (req, res, next) => {
+  const {
+    title,
+    direction,
+    description,
+    type,
+    category,
+    image,
+    latitude,
+    longitude,
+  } = req.body;
+
+  Posts.push({
+    title,
+    direction,
+    description,
+    type,
+    category,
+    image,
+    latitude,
+    longitude,
+    author: req.currentUserId,
+  });
+
+  res.send(Posts)
+};
 module.exports.getPosts = (req, res, next) => {
- res.send(mydata)
+  res.send(Posts);
+};
+
+module.exports.detail = (req, res, next) => {
+  const { id } = req.params;
+  const postId = Posts.find((post) => post.id === id);
+  res.send(postId);
 };
